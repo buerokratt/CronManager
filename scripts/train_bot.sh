1f6793ef0d10eae6d3a281105806c8632037a6b7
KlviG <78801020+KlviG@users.noreply.github.com>
2025-02-10 12:15:38 +0200

    Update train_bot.sh

--- scripts/train_bot.sh
+++ scripts/train_bot.sh
@@ -4,6 +4,10 @@ pwd
 echo $(date -u +"%Y-%m-%d %H:%M:%S.%3NZ") - $script_name started
 . constants.ini
 
+rm temp
+rm temp2
+rm temp3
+
 # POST request to merge training yaml files
 curl -X POST -H "Content-Type: application/json" -d '{"file_path":"'$TRAINING_FILES_PATH'"}' "$TRAINING_DMAPPER/mergeYaml" > temp
 
@@ -36,51 +40,51 @@ if [ "$train_status" != "200" ]; then
     exit 1
 fi
 
-if $test; then
-# PUT request to load currently trained model in RASA
-load_status=$(curl -s -w "%{http_code}" -X PUT -H "Content-Type: application/json" -d '{"model_file":"/app/models/'$trained_model_filename'"}' "$TRAINING_RASA/model")
-if [ "$load_status" != "204" ]; then
-    echo "Model loading failed with status code $load_status"
-    error_res=$(curl -H "x-ruuter-skip-authentication: true" "$TRAINING_PUBLIC_RUUTER/rasa/model/add-new-model-error")
-    echo $(date -u +"%Y-%m-%d %H:%M:%S.%3NZ") - $error_res
-    exit 1
-fi
+#if $test; then
+## PUT request to load currently trained model in RASA
+#load_status=$(curl -s -w "%{http_code}" -X PUT -H "Content-Type: application/json" -d '{"model_file":"/app/models/'$trained_model_filename'"}' "$TRAINING_RASA/model")
+#if [ "$load_status" != "204" ]; then
+#    echo "Model loading failed with status code $load_status"
+#    error_res=$(curl -H "x-ruuter-skip-authentication: true" "$TRAINING_PUBLIC_RUUTER/rasa/model/add-new-model-error")
+#    echo $(date -u +"%Y-%m-%d %H:%M:%S.%3NZ") - $error_res
+#    exit 1
+#fi
 
-testing_res=$(curl -H "x-ruuter-skip-authentication: true" "$TRAINING_PUBLIC_RUUTER/rasa/model/add-new-model-testing")
-echo $(date -u +"%Y-%m-%d %H:%M:%S.%3NZ") - $testing_res
+#testing_res=$(curl -H "x-ruuter-skip-authentication: true" "$TRAINING_PUBLIC_RUUTER/rasa/model/add-new-model-testing")
+#echo $(date -u +"%Y-%m-%d %H:%M:%S.%3NZ") - $testing_res
 
-# POST request to merge testing yaml files
-test_yaml=$(curl -X POST -H "Content-Type: application/json" -d '{"file_path":"'$TESTING_FILES_PATH'"}' "$TRAINING_DMAPPER/mergeYaml")
+## POST request to merge testing yaml files
+#test_yaml=$(curl -X POST -H "Content-Type: application/json" -d '{"file_path":"'$TESTING_FILES_PATH'"}' "$TRAINING_DMAPPER/mergeYaml")
 
-# POST request to test model in RASA
-test_response=$(curl -s -w "%{http_code}" -X POST -d "$test_yaml" "$TRAINING_RASA/model/test/stories")
-test_status="${test_response: -3}"
-if [ "$test_status" != "200" ]; then
-    echo "Model testing failed with status code $test_status"
-    error_res=$(curl -H "x-ruuter-skip-authentication: true" "$TRAINING_PUBLIC_RUUTER/rasa/model/add-new-model-error")
-    echo $(date -u +"%Y-%m-%d %H:%M:%S.%3NZ") - $error_res
-    exit 1
-fi
-test_body="${test_response:: -3}"
+## POST request to test model in RASA
+#test_response=$(curl -s -w "%{http_code}" -X POST -d "$test_yaml" "$TRAINING_RASA/model/test/stories")
+#test_status="${test_response: -3}"
+#if [ "$test_status" != "200" ]; then
+#    echo "Model testing failed with status code $test_status"
+#    error_res=$(curl -H "x-ruuter-skip-authentication: true" "$TRAINING_PUBLIC_RUUTER/rasa/model/add-new-model-error")
+#    echo $(date -u +"%Y-%m-%d %H:%M:%S.%3NZ") - $error_res
+#    exit 1
+#fi
+#test_body="${test_response:: -3}"
 
-cv_res=$(curl -H "x-ruuter-skip-authentication: true" "$TRAINING_PUBLIC_RUUTER/rasa/model/add-new-model-cross-validating")
-echo $(date -u +"%Y-%m-%d %H:%M:%S.%3NZ") - $cv_res
+#cv_res=$(curl -H "x-ruuter-skip-authentication: true" "$TRAINING_PUBLIC_RUUTER/rasa/model/add-new-model-cross-validating")
+#echo $(date -u +"%Y-%m-%d %H:%M:%S.%3NZ") - $cv_res
 
-# POST request to merge cross validating yaml files
-curl -X POST -H "Content-Type: application/json" -d '{"file_path":"'$CROSS_VALIDATION_FILES_PATH'"}' "$TRAINING_DMAPPER/mergeYaml" > temp2
+## POST request to merge cross validating yaml files
+#curl -X POST -H "Content-Type: application/json" -d '{"file_path":"'$CROSS_VALIDATION_FILES_PATH'"}' "$TRAINING_DMAPPER/mergeYaml" > temp2
 
-# POST request to cross validate model in RASA
-cross_validate_response=$(curl -s -w "%{http_code}" -X POST -H "Content-Type: application/x-yaml" --data-binary @temp2 "$TRAINING_RASA/model/test/intents?cross_validation_folds=2")
-cross_validate_status="${cross_validate_response: -3}"
+## POST request to cross validate model in RASA
+#cross_validate_response=$(curl -s -w "%{http_code}" -X POST -H "Content-Type: application/x-yaml" --data-binary @temp2 "$TRAINING_RASA/model/test/intents?cross_validation_folds=2")
+#cross_validate_status="${cross_validate_response: -3}"
 
-if [ "$cross_validate_status" != "200" ]; then
-    echo "Model cross validating failed with status code $cross_validate_status"
-    error_res=$(curl -H "x-ruuter-skip-authentication: true" "$TRAINING_PUBLIC_RUUTER/rasa/model/add-new-model-error")
-    echo $(date -u +"%Y-%m-%d %H:%M:%S.%3NZ") - $error_res
-    exit 1
-fi
-cross_validate_body="${cross_validate_response:: -3}"
-fi
+#if [ "$cross_validate_status" != "200" ]; then
+#    echo "Model cross validating failed with status code $cross_validate_status"
+#    error_res=$(curl -H "x-ruuter-skip-authentication: true" "$TRAINING_PUBLIC_RUUTER/rasa/model/add-new-model-error")
+#    echo $(date -u +"%Y-%m-%d %H:%M:%S.%3NZ") - $error_res
+#    exit 1
+#fi
+#cross_validate_body="${cross_validate_response:: -3}"
+#fi
 
 copy_file_body_dto='{"destinationFilePath":"'$trained_model_filename'","destinationStorageType":"S3","sourceFilePath":"'$trained_model_filename'","sourceStorageType":"FS"}'
 copy_file_response=$(curl -s -w "%{http_code}" -X POST -H "Content-Type: application/json" -d "$copy_file_body_dto" "$S3_FERRY_TRAIN/v1/files/copy")
@@ -106,6 +110,4 @@ echo $ready_res
 echo $(date -u +"%Y-%m-%d %H:%M:%S.%3NZ") - $ready_res
 
 rm /data/$trained_model_filename
-rm temp
-rm temp2
 echo $(date -u +"%Y-%m-%d %H:%M:%S.%3NZ") - $script_name finished
