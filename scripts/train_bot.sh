62f22f42b4d3dd0b7f886bc2ce699d68a1aff2f7
KlviG <78801020+KlviG@users.noreply.github.com>
2025-07-04 13:02:15 +0300

    Update train_bot.sh

--- scripts/train_bot.sh
+++ scripts/train_bot.sh
@@ -5,7 +5,7 @@ echo $(date -u +"%Y-%m-%d %H:%M:%S.%3NZ") - $script_name started
 . constants.ini
 
 get_new_nonce() {
-  response=$(curl -s -X POST -H "Content-Type: application/json" "$TRAINING_RESQL/get-new-nonce")
+  response=$(curl -s -X POST -H "Content-Type: application/json" "$TRAINING_RESQL/security/create_nonce")
   echo "$response"
   nonce=$(echo "$response" |grep -Eo "([a-f0-9-]+-){4}[a-f0-9-]+")
   echo "$nonce"
