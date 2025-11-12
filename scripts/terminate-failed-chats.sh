7abc60fb2614bda967a17f556a1c88021b336f9a
ffrose <119657383+ffrose@users.noreply.github.com>
2025-11-12 14:53:25 +0200

    Update terminate-failed-chats.sh

--- scripts/terminate-failed-chats.sh
+++ scripts/terminate-failed-chats.sh
@@ -9,10 +9,10 @@ currentTimestamp() {
 
 echo "$(currentTimestamp) - $script_name started"
 
-#.. /app/constants.ini
+../app/constants.ini
 
 get_new_nonce() {
-  response=$(curl -s -X POST -H "Content-Type: application/json" "http://component-byk-resql:8082/training/get-new-nonce")
+  response=$(curl -s -X POST -H "Content-Type: application/json" "$TRAINING_RESQL/get-new-nonce")
   nonce=$(echo "$response" |grep -Eo "([a-f0-9-]+-){4}[a-f0-9-]+")
   echo "$nonce"
 }
@@ -20,7 +20,7 @@ get_new_nonce() {
 dead_chat_ids=$(curl -s \
   -H "x-ruuter-nonce: $(get_new_nonce)" \
   -H "Content-Type: application/json" \
-  "http://component-byk-ruuter-private:8080/backoffice/cron-tasks/end-dead-chats")
+  "$CHATBOT_RUUTER_PRIVATE/cron-tasks/end-dead-chats")
 
 echo "$(date -u +"%Y-%m-%d %H:%M:%S.%3NZ") - Raw Response: $dead_chat_ids"
 
@@ -29,7 +29,7 @@ ids=$(echo "$dead_chat_ids" | jq -r '.response[]')
 if [ -n "$ids" ]; then
   for id in $ids; do
     echo "$(date -u +"%Y-%m-%d %H:%M:%S.%3NZ") - Ending chat $id"
-    curl -s -X POST "http://component-byk-ruuter:8080/backoffice/chats/end" \
+    curl -s -X POST "$CHATBOT_RUUTER_PUBLIC/chats/end" \
       -H "Content-Type: application/json" \
       -d "{
         \"message\": {
