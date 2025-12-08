5caab4d735b73d65e14f159a607dab991ba04f31
Janno Stern <jannostern@users.noreply.github.com>
2025-12-08 12:14:23 +0200

    Implement Jira sync

--- scripts/create_jira_issue.sh
+++ scripts/create_jira_issue.sh
@@ -4,8 +4,13 @@ pwd
 echo $(date -u +"%Y-%m-%d %H:%M:%S.%3NZ") - $script_name started
 . constants.ini
 
+if [ "$JIRA_INTEGRATION_ENABLED" = "false" ]; then
+  echo "$(date -u +"%Y-%m-%d %H:%M:%S.%3NZ") - $script_name exiting: JIRA integration disabled"
+  exit 0
+fi
+
 get_new_nonce() {
-  response=$(curl -s -X POST -H "Content-Type: application/json" "$TRAINING_RESQL/get-new-nonce")
+  response=$(curl -s -X POST -H "Content-Type: application/json" "$CHATBOT_TRAINING_RESQL/get-new-nonce")
   nonce=$(echo "$response" |grep -Eo "([a-f0-9-]+-){4}[a-f0-9-]+")
   echo "$nonce"
 }
