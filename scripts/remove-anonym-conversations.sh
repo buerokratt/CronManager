d413c1ec747c1e1915987ce6a0d4145d1881b6b6
1AhmedYasser <26207361+1AhmedYasser@users.noreply.github.com>
2026-05-07 15:20:41 +0300

    chore(855): Modify nonce path

--- scripts/remove-anonym-conversations.sh
+++ scripts/remove-anonym-conversations.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${SCRIPT_DIR}/../constants.ini"
 
 get_new_nonce() {
-  response=$(curl -s -X POST -H "Content-Type: application/json" "$CHATBOT_BOT_RESQL/get-new-nonce")
+  response=$(curl -s -X POST -H "Content-Type: application/json" "$TRAINING_RESQL/get-new-nonce")
   nonce=$(echo "$response" |grep -Eo "([a-f0-9-]+-){4}[a-f0-9-]+")
   echo "$nonce"
 }
